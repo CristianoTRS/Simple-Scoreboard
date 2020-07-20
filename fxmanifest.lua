445c9f71cc86e3291c9dbb9ce410d72bd08be155
CristianoTRS <50265039+CristianoTRS@users.noreply.github.com>
2020-07-20 21:03:12 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'CristianoTRS#5521'
+
+description 'Just a simple and clean scoreboard made with Bootstrap :D'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'server.lua'
+}
+
+files {
+    'html/*'
+}
